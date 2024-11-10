c5953a60cd62e2c8fc0a63e6f8df6f80bb90c358
ChrisHughes24 <chrishughes24@gmail.com>
2024-11-10 14:08:28 +0000

    split off forMathlib file

--- Quantelim/Basic.lean
+++ Quantelim/Basic.lean
@@ -4,105 +4,6 @@ import Mathlib.RingTheory.Polynomial.Basic
 import Mathlib.Algebra.Polynomial.Derivative
 import Mathlib.Order.PiLex
 import Mathlib.Data.Vector.Basic
-import Mathlib.Data.Complex.Basic
-
-namespace Polynomial
-
-open Polynomial
-
-variable {K : Type*} [Field K]
-
-theorem Splits.dvd_of_roots_le_roots {p q : K[X]} (hp : p.Splits (RingHom.id _)) (hp0 : p ≠ 0)
-    (hq : p.roots ≤ q.roots) : p ∣ q := by
-  by_cases hq0 : q = 0
-  · simp [hq0]
-  · rw [eq_prod_roots_of_splits_id hp, C_mul_dvd (leadingCoeff_ne_zero.2 hp0)]
-    exact dvd_trans (Multiset.prod_dvd_prod_of_le
-           (Multiset.map_le_map hq)) (prod_multiset_X_sub_C_dvd _)
-
-theorem Splits.dvd_iff_roots_le_roots {p q : K[X]}
-    (hp : p.Splits (RingHom.id _)) (hp0 : p ≠ 0) (hq0 : q ≠ 0) :
-    p ∣ q ↔ p.roots ≤ q.roots := by
-  refine ⟨?_, Splits.dvd_of_roots_le_roots hp hp0⟩
-  · rintro ⟨r, rfl⟩
-    rw [roots_mul hq0]; exact le_add_right le_rfl
-
-theorem rootMultiplicity_derivative {R : Type*} [CommRing R] [CharZero R] [IsDomain R]
-    {p : R[X]} {t : R} (hpt : Polynomial.IsRoot p t) :
-    (derivative p).rootMultiplicity t = p.rootMultiplicity t - 1 := by
-  by_cases hp0 : p = 0
-  · simp [hp0]
-  · exact derivative_rootMultiplicity_of_root_of_mem_nonZeroDivisors hpt
-      (mem_nonZeroDivisors_iff_ne_zero.2 (Nat.cast_ne_zero.2 (Nat.ne_zero_iff_zero_lt.2
-        ((rootMultiplicity_pos hp0).2 hpt))))
-
-theorem key_one_way {R : Type*} [CommRing R] [IsDomain R] [CharZero R] {f g : R[X]} (hf0 : f ≠ 0)
-    (hfd : f ∣ f.derivative * g) : ∀ x, f.eval x = 0 → g.eval x = 0 := by
-  intro a haf
-  rcases hfd with ⟨r, hr⟩
-  by_cases hdf0 : derivative f = 0
-  · have := natDegree_eq_zero_of_derivative_eq_zero hdf0
-    rw [eq_C_of_derivative_eq_zero hdf0] at haf
-    simp only [eval_C, derivative_C, zero_mul, dvd_zero, iff_true] at haf
-    refine False.elim (hf0 (Polynomial.ext ?_))
-    intro n
-    cases n
-    · rw [haf]; simp
-    · rw [eq_C_of_derivative_eq_zero hdf0]; simp
-  by_contra hg
-  have hdfg0 : f.derivative * g ≠ 0 := mul_ne_zero hdf0 (by rintro rfl; simp_all)
-  have hr' := congr_arg (rootMultiplicity a) hr
-  rw [rootMultiplicity_mul hdfg0, derivative_rootMultiplicity_of_root haf,
-    rootMultiplicity_eq_zero hg, add_zero, rootMultiplicity_mul (hr ▸ hdfg0), add_comm,
-    Nat.sub_eq_iff_eq_add (Nat.succ_le_iff.2 ((rootMultiplicity_pos hf0).2 haf))] at hr'
-  refine lt_irrefl (rootMultiplicity a f) ?_
-  refine lt_of_lt_of_le (Nat.lt_succ_self _)
-    (le_trans (le_add_of_nonneg_left (Nat.zero_le (rootMultiplicity a r))) ?_)
-  conv_rhs => rw [hr']
-  simp [add_assoc]
-
-theorem key {f g : ℂ[X]} (hf0 : f ≠ 0) :
-    (∀ x, f.eval x = 0 → g.eval x = 0) ↔ f ∣ f.derivative * g := by
-  by_cases hg0 : g = 0
-  · simp [hg0]
-  by_cases hdf0 : derivative f = 0
-  · rw [eq_C_of_derivative_eq_zero hdf0]
-    simp only [eval_C, derivative_C, zero_mul, dvd_zero, iff_true]
-    intro x hx
-    refine False.elim (hf0 (Polynomial.ext ?_))
-    intro n
-    cases n
-    · rw [hx]; simp
-    · rw [eq_C_of_derivative_eq_zero hdf0]; simp
-  have hdg :  f.derivative * g ≠ 0 := mul_ne_zero hdf0 hg0
-  rw [Splits.dvd_iff_roots_le_roots (IsAlgClosed.splits f) hf0 hdg, Multiset.le_iff_count]
-  simp only [count_roots, rootMultiplicity_mul hdg]
-  refine forall_congr' fun a => ?_
-  by_cases haf : f.eval a = 0
-  · have h0 : 0 < f.rootMultiplicity a := (rootMultiplicity_pos hf0).2 haf
-    rw [derivative_rootMultiplicity_of_root haf]
-    refine ⟨?_, ?_⟩
-    · intro h
-      calc rootMultiplicity a f
-          = rootMultiplicity a f - 1 + 1 := (Nat.sub_add_cancel (Nat.succ_le_iff.1 h0)).symm
-        _ ≤ rootMultiplicity a f - 1 + rootMultiplicity a g := add_le_add le_rfl (Nat.succ_le_iff.1
-          ((rootMultiplicity_pos hg0).2 (h haf)))
-    · intro h haf
-      by_contra hg
-      rw [rootMultiplicity_eq_zero hg, add_zero, Nat.le_sub_iff_add_le (Nat.succ_le_iff.1 h0)] at h
-      exact not_le_of_gt (Nat.lt_succ_self _) h
-  · simp [haf, rootMultiplicity_eq_zero haf]
-
-theorem square_free_key {R : Type*} [CommRing R] [IsDomain R] [CharZero R]
-    {f g a : R[X]} {hf0 : f ≠ 0} (hgf : g * a = f) (hgdf : g ∣ f.derivative) :
-    (∀ x, f.eval x = 0 ↔ a.eval x = 0) := by
-  have hg0 : g ≠ 0 := by rintro rfl; simp_all
-  subst hgf
-  rw [derivative_mul, dvd_add_left (dvd_mul_right _ _)] at hgdf
-  have := key_one_way hg0 hgdf
-  simpa only [eval_mul, mul_eq_zero, or_iff_right_iff_imp]
-
-end Polynomial
 
 variable {K : Type*} [CommRing K]
 
@@ -404,47 +305,44 @@ noncomputable def toMvPoly (p : Poly n) : MvPolynomial (Fin n) ℤ := eval p MvP
 noncomputable def toPoly (p : Poly (n+1)) : Polynomial (MvPolynomial (Fin n) ℤ) :=
   eval p (Fin.cons Polynomial.X (fun i => Polynomial.C (MvPolynomial.X i)))
 
-mutual
-
-def gcd : ∀ {n : ℕ} (p q : Poly n),
-    Poly n × --the gcd
-    Poly n × --p / gcd
-    Poly n -- q / gcd
-  | 0, ofInt' x, ofInt' y => ⟨(Int.gcd x y : ℤ),
-    (x / Int.gcd x y : ℤ), (y / Int.gcd x y : ℤ)⟩
-  | n+1, _, _ => sorry
-
-
-/-- returns `(h, d)` such that `q.leadingCoeff ^ (degree p - degree q + 1) * p = h * q + r`,
-  assuming `degree p ≤ degree q` -/
-def pseudoModDiv (p q : Poly (n+1)) : (Poly (n+1) × Poly (n+1)) :=
-  let dp := degree p
-  let dq := degree q
-  if dp ≤ dq
-  then
-    let z := const (leadingCoeff p) * X ^ (dp - dq)
-    let dm := pseudoModDiv (const q.leadingCoeff * p - q * z) q
-    ⟨z + dm.1, dm.2⟩
-  else ⟨0, p⟩
-
-end
-
-
--- letI := Classical.decEq R
---     if h : degree q ≤ degree p ∧ p ≠ 0 then
---       let z := C (leadingCoeff p) * X ^ (natDegree p - natDegree q)
---       have _wf := div_wf_lemma h hq
---       let dm := divModByMonicAux (p - q * z) hq
---       ⟨z + dm.1, dm.2⟩
---     else ⟨0, p⟩
---   termination_by p => p
-
-
-
-
-
 
 end Poly
+
+-- mutual
+
+-- def gcd : ∀ {n : ℕ} (p q : Poly n),
+--     Poly n × --the gcd
+--     Poly n × --p / gcd
+--     Poly n -- q / gcd
+--   | 0, ofInt' x, ofInt' y => ⟨(Int.gcd x y : ℤ),
+--     (x / Int.gcd x y : ℤ), (y / Int.gcd x y : ℤ)⟩
+--   | n+1, _, _ => sorry
+
+
+-- /-- returns `(h, d)` such that `q.leadingCoeff ^ (degree p - degree q + 1) * p = h * q + r`,
+--   assuming `degree p ≤ degree q` -/
+-- -- def pseudoModDiv (p q : Poly (n+1)) : (Poly (n+1) × Poly (n+1)) :=
+-- --   let dp := degree p
+-- --   let dq := degree q
+-- --   if dp ≤ dq
+-- --   then
+-- --     let z := const (leadingCoeff p) * X ^ (dp - dq)
+-- --     let dm := pseudoModDiv (const q.leadingCoeff * p - q * z) q
+-- --     ⟨z + dm.1, dm.2⟩
+-- --   else ⟨0, p⟩
+
+-- --end
+
+
+-- -- letI := Classical.decEq R
+-- --     if h : degree q ≤ degree p ∧ p ≠ 0 then
+-- --       let z := C (leadingCoeff p) * X ^ (natDegree p - natDegree q)
+-- --       have _wf := div_wf_lemma h hq
+-- --       let dm := divModByMonicAux (p - q * z) hq
+-- --       ⟨z + dm.1, dm.2⟩
+-- --     else ⟨0, p⟩
+-- --   termination_by p => p
+
 
 open Poly
 
---
+++ Quantelim/forMathlib.lean
@@ -0,0 +1,93 @@
+import Mathlib.FieldTheory.IsAlgClosed.Basic
+import Mathlib.RingTheory.Polynomial.Basic
+import Mathlib.Algebra.Polynomial.Splits
+import Mathlib.Algebra.Polynomial.Derivative
+
+namespace Polynomial
+
+open Polynomial
+
+variable {K : Type*} [Field K]
+
+theorem Splits.dvd_of_roots_le_roots {p q : K[X]} (hp : p.Splits (RingHom.id _)) (hp0 : p ≠ 0)
+    (hq : p.roots ≤ q.roots) : p ∣ q := by
+  by_cases hq0 : q = 0
+  · simp [hq0]
+  · rw [eq_prod_roots_of_splits_id hp, C_mul_dvd (leadingCoeff_ne_zero.2 hp0)]
+    exact dvd_trans (Multiset.prod_dvd_prod_of_le
+           (Multiset.map_le_map hq)) (prod_multiset_X_sub_C_dvd _)
+
+theorem Splits.dvd_iff_roots_le_roots {p q : K[X]}
+    (hp : p.Splits (RingHom.id _)) (hp0 : p ≠ 0) (hq0 : q ≠ 0) :
+    p ∣ q ↔ p.roots ≤ q.roots := by
+  refine ⟨?_, Splits.dvd_of_roots_le_roots hp hp0⟩
+  · rintro ⟨r, rfl⟩
+    rw [roots_mul hq0]; exact le_add_right le_rfl
+
+theorem rootMultiplicity_derivative {R : Type*} [CommRing R] [CharZero R] [IsDomain R]
+    {p : R[X]} {t : R} (hpt : Polynomial.IsRoot p t) :
+    (derivative p).rootMultiplicity t = p.rootMultiplicity t - 1 := by
+  by_cases hp0 : p = 0
+  · simp [hp0]
+  · exact derivative_rootMultiplicity_of_root_of_mem_nonZeroDivisors hpt
+      (mem_nonZeroDivisors_iff_ne_zero.2 (Nat.cast_ne_zero.2 (Nat.ne_zero_iff_zero_lt.2
+        ((rootMultiplicity_pos hp0).2 hpt))))
+
+theorem key_one_way {R : Type*} [CommRing R] [IsDomain R] [CharZero R] {f g : R[X]} (hf0 : f ≠ 0)
+    (hfd : f ∣ f.derivative * g) : ∀ x, f.eval x = 0 → g.eval x = 0 := by
+  intro a haf
+  rcases hfd with ⟨r, hr⟩
+  by_cases hdf0 : derivative f = 0
+  · have := natDegree_eq_zero_of_derivative_eq_zero hdf0
+    rw [eq_C_of_derivative_eq_zero hdf0] at haf
+    simp only [eval_C, derivative_C, zero_mul, dvd_zero, iff_true] at haf
+    refine False.elim (hf0 (Polynomial.ext ?_))
+    intro n
+    cases n
+    · rw [haf]; simp
+    · rw [eq_C_of_derivative_eq_zero hdf0]; simp
+  by_contra hg
+  have hdfg0 : f.derivative * g ≠ 0 := mul_ne_zero hdf0 (by rintro rfl; simp_all)
+  have hr' := congr_arg (rootMultiplicity a) hr
+  rw [rootMultiplicity_mul hdfg0, derivative_rootMultiplicity_of_root haf,
+    rootMultiplicity_eq_zero hg, add_zero, rootMultiplicity_mul (hr ▸ hdfg0), add_comm,
+    Nat.sub_eq_iff_eq_add (Nat.succ_le_iff.2 ((rootMultiplicity_pos hf0).2 haf))] at hr'
+  refine lt_irrefl (rootMultiplicity a f) ?_
+  refine lt_of_lt_of_le (Nat.lt_succ_self _)
+    (le_trans (le_add_of_nonneg_left (Nat.zero_le (rootMultiplicity a r))) ?_)
+  conv_rhs => rw [hr']
+  simp [add_assoc]
+
+variable {K : Type*} [Field K] [CharZero K] [IsAlgClosed K]
+theorem key {f g : K[X]} (hf0 : f ≠ 0) :
+    (∀ x, f.eval x = 0 → g.eval x = 0) ↔ f ∣ f.derivative * g := by
+  refine ⟨?_, key_one_way hf0⟩
+  by_cases hg0 : g = 0
+  · simp [hg0]
+  by_cases hdf0 : derivative f = 0
+  · rw [eq_C_of_derivative_eq_zero hdf0]
+    simp only [eval_C, derivative_C, zero_mul, dvd_zero, implies_true]
+  have hdg :  f.derivative * g ≠ 0 := mul_ne_zero hdf0 hg0
+  classical rw [Splits.dvd_iff_roots_le_roots (IsAlgClosed.splits f) hf0 hdg, Multiset.le_iff_count]
+  simp only [count_roots, rootMultiplicity_mul hdg]
+  refine forall_imp fun a => ?_
+  by_cases haf : f.eval a = 0
+  · have h0 : 0 < f.rootMultiplicity a := (rootMultiplicity_pos hf0).2 haf
+    rw [derivative_rootMultiplicity_of_root haf]
+    intro h
+    calc rootMultiplicity a f
+        = rootMultiplicity a f - 1 + 1 := (Nat.sub_add_cancel (Nat.succ_le_iff.1 h0)).symm
+      _ ≤ rootMultiplicity a f - 1 + rootMultiplicity a g := add_le_add le_rfl (Nat.succ_le_iff.1
+        ((rootMultiplicity_pos hg0).2 (h haf)))
+  · simp [haf, rootMultiplicity_eq_zero haf]
+
+theorem square_free_key {R : Type*} [CommRing R] [IsDomain R] [CharZero R]
+    {f g a : R[X]} {hf0 : f ≠ 0} (hgf : g * a = f) (hgdf : g ∣ f.derivative) :
+    (∀ x, f.eval x = 0 ↔ a.eval x = 0) := by
+  have hg0 : g ≠ 0 := by rintro rfl; simp_all
+  subst hgf
+  rw [derivative_mul, dvd_add_left (dvd_mul_right _ _)] at hgdf
+  have := key_one_way hg0 hgdf
+  simpa only [eval_mul, mul_eq_zero, or_iff_right_iff_imp]
+
+end Polynomial
