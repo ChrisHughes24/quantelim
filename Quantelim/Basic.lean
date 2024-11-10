37ab1a99a131752ab1371ae0e80220aafd7f1534
ChrisHughes24 <chrishughes24@gmail.com>
2024-11-10 10:23:07 +0000

    improve definition of Ands

--- Quantelim/Basic.lean
+++ Quantelim/Basic.lean
@@ -151,27 +151,30 @@ def ofInt : ∀ {n : ℕ}, ℤ  → Poly n
 instance {n : ℕ} : IntCast (Poly n) := ⟨ofInt⟩
 
 @[simp]
-theorem eval_ratCast : ∀ {n : ℕ} (x : ℤ ) (vars : Fin n → K),
-    eval (x : Poly n) vars = x
+theorem eval_ofInt : ∀ {n : ℕ} (x : ℤ) (vars : Fin n → K),
+    eval (ofInt x) vars = x
   | 0, _, _ => by simp [eval]
   | n+1, x, vars => by
-    simp only [eval]
-    rw [← @eval_ratCast n]
-    rfl
+    rw [ofInt, eval, eval_ofInt]
+
+@[simp]
+theorem eval_intCast : ∀ {n : ℕ} (x : ℤ) (vars : Fin n → K),
+    eval (x : Poly n) vars = x :=
+  eval_ofInt
 
 instance {n : ℕ} : Zero (Poly n) := ⟨(0 : ℤ )⟩
 
 @[simp]
 theorem eval_zero {n : ℕ} (vars : Fin n → K) :
     eval (0 : Poly n) vars = 0 := by
-  erw [eval_ratCast 0]; simp
+  erw [eval_intCast 0]; simp
 
 instance {n : ℕ} : One (Poly n) := ⟨(1 : ℤ )⟩
 
 @[simp]
 theorem eval_one {n : ℕ} (vars : Fin n → K) :
     eval (1 : Poly n) vars = 1 := by
-  erw [eval_ratCast 1]; simp
+  erw [eval_intCast 1]; simp
 
 def constAddXMul' {n : ℕ} (p : Poly n) (q : Poly (n + 1)) : Poly (n+1) :=
   if q = 0 then const p else constAddXMul p q
@@ -183,7 +186,6 @@ theorem eval_constAddXMul' {n : ℕ} (p : Poly n) (q : Poly (n + 1)) (vars : Fin
   split_ifs <;>
   simp_all [eval]
 
-@[simp]
 def add : {n : ℕ} → Poly n → Poly n → Poly n
   | _, ofInt' x, ofInt' y => ofInt' (x + y)
   | _, const p, const q => const (add p q)
@@ -198,17 +200,16 @@ theorem eval_add' : {n : ℕ} → (p : Poly n) → (q : Poly n) → (vars : Fin 
     eval (p.add q) vars = p.eval vars + q.eval vars
   | _, ofInt' x, ofInt' y => by simp
   | _, const p, const q => by simp [eval_add' p q]
-  | _, const p, constAddXMul q r => by simp [eval_add' p q, add_comm, add_assoc, add_left_comm]
-  | _, constAddXMul p q, const r => by simp [eval_add' p r, add_comm, add_assoc, add_left_comm]
+  | _, const p, constAddXMul q r => by simp [eval_add' p q, add_comm, add_assoc, add_left_comm, add]
+  | _, constAddXMul p q, const r => by simp [eval_add' p r, add_comm, add_assoc, add_left_comm, add]
   | _, constAddXMul p q, constAddXMul r s => by
-    simp [eval_add' p r, eval_add' q s, add_comm, add_assoc, add_left_comm, mul_add]
+    simp [eval_add' p r, eval_add' q s, add_comm, add_assoc, add_left_comm, mul_add, add]
 
 @[simp]
 theorem eval_add : {n : ℕ} → (p : Poly n) → (q : Poly n) → (vars : Fin n → K) →
     eval (p + q) vars = p.eval vars + q.eval vars :=
   eval_add'
 
-@[simp]
 def mul : {n : ℕ} → Poly n → Poly n → Poly n
   | _, ofInt' x, ofInt' y => ofInt' (x * y)
   | _, const p, const q => const (mul p q)
@@ -224,14 +225,14 @@ instance {n : ℕ} : Mul (Poly n) := ⟨Poly.mul⟩
 @[simp]
 theorem eval_mul' : {n : ℕ} → (p : Poly n) → (q : Poly n) → (vars : Fin n → K) →
     eval (p.mul q) vars = p.eval vars * q.eval vars
-  | _, ofInt' x, ofInt' y, _ => by simp
-  | _, const p, const q, _ => by simp [eval_mul' p q]
+  | _, ofInt' x, ofInt' y, _ => by simp [mul]
+  | _, const p, const q, _ => by simp [eval_mul' p q, mul]
   | _, const p, constAddXMul q r, _ => by
-    simp [eval_mul' p q, eval_mul' (const p) r]; ring
+    simp [eval_mul' p q, eval_mul' (const p) r, mul]; ring
   | _, constAddXMul p q, const r, _ => by
-    simp [eval_mul' p r, eval_mul' q (const r)]; ring
+    simp [eval_mul' p r, eval_mul' q (const r), mul]; ring
   | _+1, constAddXMul p q, constAddXMul r s, _ => by
-    simp [eval_mul' p r, eval_mul' (const p) s, eval_mul' q (const r), eval_mul' q s]
+    simp [eval_mul' p r, eval_mul' (const p) s, eval_mul' q (const r), eval_mul' q s, mul]
     ring
   termination_by n p q => (n, sizeOf p + sizeOf q)
 
@@ -240,13 +241,19 @@ theorem eval_mul : {n : ℕ} → (p : Poly n) → (q : Poly n) → (vars : Fin n
     eval (p * q) vars = p.eval vars * q.eval vars :=
   eval_mul'
 
-@[simp]
 def neg : {n : ℕ} → (p : Poly n) → Poly n
   | _, ofInt' x => ofInt' (-x)
   | _, const p => const (neg p)
   | _, constAddXMul p q => constAddXMul (neg p) (neg q)
 
 instance {n : ℕ} : Neg (Poly n) := ⟨Poly.neg⟩
+
+theorem neg_const {n : ℕ} (p : Poly n) :
+    -const p = const (-p) := rfl
+
+@[simp]
+theorem constAddXMul_neg {n : ℕ} (p : Poly n) (q : Poly (n+1)) :
+    -constAddXMul p q = constAddXMul (-p) (-q) := rfl
 
 @[simp]
 theorem eval_neg' {n : ℕ} (p : Poly n) : (vars : Fin n → K) →
@@ -269,6 +276,11 @@ def leadingCoeff : ∀ {n : ℕ}, Poly (n+1) → Poly n
   | _, const p => p
   | _, constAddXMul _ q => leadingCoeff q
 
+def eraseLead : ∀ {n : ℕ}, Poly n → Poly n
+  | _, ofInt' _ => 0
+  | _, const _ => 0
+  | _, constAddXMul p q => constAddXMul' p (eraseLead q)
+
 open Mathlib Mathlib.Vector
 
 def leadingMon : ∀ {n : ℕ}, Poly n → Vector ℕ n
@@ -289,19 +301,72 @@ def deriv : ∀ {n : ℕ}, Poly (n + 1) → Poly (n + 1)
 
 def X : Poly (n + 1) := constAddXMul 0 1
 
+@[simp]
+theorem eval_X {n : ℕ} (vars : Fin (n+1) → K) :
+    eval (X : Poly (n+1)) vars = vars 0 := by
+  simp [X, eval]
+
 instance {n : ℕ} : NatPow (Poly n) := ⟨fun p n => (.*p)^[n] 1⟩
 
--- def gcd : ∀ {n : ℕ}, (p q : Poly n) →
---     Poly n × --the gcd
---     Poly n × --p / gcd
---     Poly n -- q / gcd
---   | _, ofInt' x, q => ⟨ofInt 1, ofInt' x, q⟩
---   | _, p, ofInt' x => ⟨ofInt 1, p, ofInt' x⟩
---   | _, const p, const q =>
---     let (g, a, b) := gcd p q
---     (const g, const a, const b)
---   | _, const p, constAddXMul r s =>
---     let (g, a, b) := gcd p r
+@[simp]
+theorem eval_pow {n : ℕ} (p : Poly n) (m : ℕ) (vars : Fin n → K) :
+    eval (p^m) vars = p.eval vars ^ m := by
+  induction m with
+  | zero => rw [pow_zero]; exact eval_one vars
+  | succ m ih =>
+    rw [pow_succ, ← ih, ← eval_mul]
+    show ((.*p)^[m+1] 1).eval vars = _
+    rw [Function.iterate_succ']
+    simp only [Function.comp_apply, eval_mul]
+    rfl
+
+-- theorem eraseLead_eq_zero_iff : ∀ {n : ℕ} (p : Poly n), p.eraseLead = 0 ↔ degree p = 0
+--   | _, ofInt' _ => by simp [degree, eraseLead]
+--   | _, const _ => by simp [degree, eraseLead]
+--   | _, constAddXMul p q => by
+--     rw [degree, eraseLead, constAddXMul']
+--     split_ifs with hq0
+--     ·
+
+theorem degree_pos_of_eraseLead_ne_zero : ∀ {n : ℕ} {p : Poly n}, p.eraseLead ≠ 0 → 0 < p.degree
+  | _, ofInt' _ => by simp [eraseLead]
+  | _, const _ => by simp [eraseLead]
+  | _, constAddXMul _ _ => by simp [degree]
+
+theorem degree_eraseLead : ∀ {n : ℕ} (p : Poly n), degree (eraseLead p) ≤ degree p - 1
+  | _, ofInt' _ => by simp [degree]
+  | _, const _ => by simp [degree]
+  | _, constAddXMul p q => by
+    rw [degree, eraseLead, constAddXMul']
+    split_ifs with hq0
+    · simp [degree]
+    · simp only [degree, add_tsub_cancel_right]
+      refine le_trans (Nat.succ_le_succ (degree_eraseLead q)) ?_
+      rw [← Nat.succ_sub (Nat.succ_le.2 (degree_pos_of_eraseLead_ne_zero hq0))]
+      simp
+
+@[simp]
+theorem degree_neg : ∀ {n : ℕ} (p : Poly n), (-p).degree = p.degree
+  | _, ofInt' _ => by simp [degree]
+  | _, const _ => by simp [degree]
+  | _, constAddXMul p q => by
+    rw [degree, ← degree_neg q]
+    rfl
+
+@[simp]
+theorem leadingCoeff_neg : ∀ {n : ℕ} (p : Poly (n+1)), (-p).leadingCoeff = -p.leadingCoeff
+  | _, const _ => by rw [leadingCoeff, neg_const, leadingCoeff]
+  | _, constAddXMul p q => by
+    rw [leadingCoeff, ← leadingCoeff_neg, constAddXMul_neg, leadingCoeff]
+
+def gcd : ∀ {n : ℕ} (p q : Poly n),
+    Poly n × --the gcd
+    Poly n × --p / gcd
+    Poly n -- q / gcd
+  | 0, ofInt' x, ofInt' y => _
+  | n+1, _, _ => sorry
+
+
 
 -- letI := Classical.decEq R
 --     if h : degree q ≤ degree p ∧ p ≠ 0 then
@@ -312,17 +377,18 @@ instance {n : ℕ} : NatPow (Poly n) := ⟨fun p n => (.*p)^[n] 1⟩
 --     else ⟨0, p⟩
 --   termination_by p => p
 
-/-- returns `(h, d)` such that `q.leadingCoeff ^ (degree p - degree q + 1) * p = h * q + r -/
-def pseudoModDiv (p q : Poly n) :
-    (Poly n × --Mod h
-    Poly n) --Div d
-  :=
+/-- returns `(h, d)` such that `q.leadingCoeff ^ (degree p - degree q + 1) * p = h * q + r`,
+  assuming `degree p ≤ degree q` -/
+def pseudoModDiv (p q : Poly (n+1)) : (Poly (n+1) × Poly (n+1)) :=
   let dp := degree p
   let dq := degree q
   if dp ≤ dq
   then
-  let z := leadingCoeff p * X ^ (dp - dq)
-  let dm := pseudoModDiv (p - q * z) q
+    let z := const (leadingCoeff p) * X ^ (dp - dq)
+    let dm := pseudoModDiv (const q.leadingCoeff * p - q * z) q
+    ⟨z + dm.1, dm.2⟩
+  else ⟨0, p⟩
+
 
 
 
@@ -333,8 +399,6 @@ open Poly
 
 -- Invariants to maintain. No constant polys in any list. Eqs has smallest by lex leadingMon degree at head
 structure Ands (n : ℕ) : Type where
-  /-- A var is nonzero iff its index is `<` than `nonzeroVars` -/
-  (nonzeroVars : Fin (n+1))
   (eqs : List (Poly (n+1)))
   (neq : Poly (n+1))
   deriving DecidableEq
