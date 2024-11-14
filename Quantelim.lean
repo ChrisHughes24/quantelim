7ead7b5d31dba47c8413fc53bdff78bad6ccdcd2
ChrisHughes24 <chrishughes24@gmail.com>
2024-11-14 08:31:30 +0000

    imports

--- Quantelim.lean
+++ Quantelim.lean
@@ -2,5 +2,6 @@ import Quantelim.Basic
 import Quantelim.Formula
 import Quantelim.Poly.Basic
 import Quantelim.Poly.Div
+import Quantelim.Poly.Zeros
 import Quantelim.QuantifierFree
 import Quantelim.forMathlib
